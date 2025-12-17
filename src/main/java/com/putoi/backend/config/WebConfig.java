86d06f5a21e78ce86ca99db9312d5627d499c1a2
Muhamad Alfian Widjaya <muhamad.alfian.widjaya@bni.co.id>
2025-12-17 15:29:12 +0700

    penambahan config pada aplication properti

--- src/main/java/com/putoi/backend/config/WebConfig.java
+++ src/main/java/com/putoi/backend/config/WebConfig.java
@@ -4,6 +4,7 @@
  */
 package com.putoi.backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -16,13 +17,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    @Value("${app.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*") // atau bisa spesifik: "Authorization, Content-Type"
                         .exposedHeaders("*")
